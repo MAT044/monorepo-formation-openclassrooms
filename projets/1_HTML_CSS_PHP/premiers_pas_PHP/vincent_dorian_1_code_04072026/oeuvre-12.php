89a4831dc2407782fa17ae562ec006309bb8bd2f
Vincent DORIAN <ress.edrakmisto@gmail.com>
2026-07-04 15:39:27 +0200

    [Projet01B-Etape02] Refactor : Remplacement des header et footer dans tous les fichiers (search&replace) Passage html -> php

--- projets/1_HTML_CSS_PHP/premiers_pas_PHP/vincent_dorian_1_code_04072026/oeuvre-12.php
+++ projets/1_HTML_CSS_PHP/premiers_pas_PHP/vincent_dorian_1_code_04072026/oeuvre-12.php
@@ -1,23 +1,4 @@
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>The ArtBox</title>
-</head>
-<body>
-<header>
-    <a href="index.html"><img src="img/logo.png" alt="Logo Artbox" id="logo"></a>
-    <nav>
-        <ul>
-            <li><a href="index.html">Accueil</a></li>
-        </ul>
-    </nav>
-</header>
-<main>
+<?php include 'includes/header.php'; ?>
     <article id="detail-oeuvre">
         <div id="img-oeuvre">
             <img src="img/fly-d.png" alt="La Galaxia Gialla">
@@ -30,11 +11,4 @@
             </p>
         </div>
     </article>
-</main>
-<footer>
-    <p>
-        <strong>© THE ARTBOX</strong> - <em>Tous droits réservés</em>
-    </p>
-</footer>
-</body>
-</html>
+<?php include 'includes/footer.php'; ?>
