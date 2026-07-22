9ed06e0c251abc726f896a8f21bc111337ee8bdd
Vincent DORIAN <ress.edrakmisto@gmail.com>
2026-07-22 08:47:22 +0200

    [Projet04-Etape01] Récupération du code de "The Artbox"

---
+++ projets/4_site_simple_php/index.php
@@ -0,0 +1,17 @@
+<?php include 'includes/header.php'; ?>
+        <div id="liste-oeuvres">
+            <?php 
+                $oeuvres = include 'data/oeuvres.php';
+                foreach($oeuvres as $oeuvre):
+            ?>
+                <article class="oeuvre">
+                    <a href="oeuvre.php?id=<?= $oeuvre['id'] ?>">
+                        <img src="img/<?= $oeuvre['img'] ?>" alt="<?= $oeuvre['title'] ?>">
+                        <h2><?= $oeuvre['title'] ?></h2>
+                        <p class="description"><?= $oeuvre['artist'] ?></p>
+                    </a>
+                </article>
+            <?php endforeach; ?>
+            
+        </div>
+<?php include 'includes/footer.php'; ?>
