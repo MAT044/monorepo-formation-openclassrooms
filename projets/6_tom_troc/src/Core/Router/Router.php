1e2f3c0e100419edd1fbdc2b6b43a09f4d325447
Vincent DORIAN <ress.edrakmisto@gmail.com>
2026-08-16 11:28:04 +0200

    [Projet06B] Feat : Implémation de findRoute basé sur preg_match

--- projets/6_tom_troc/src/Core/Router/Router.php
+++ projets/6_tom_troc/src/Core/Router/Router.php
@@ -2,21 +2,47 @@
 
 namespace TomTroc\Core\Router;
 
+use Exception;
+use Throwable;
 use TomTroc\Controller\HomeController;
 use TomTroc\Core\Http\Method;
 use TomTroc\Core\Http\Request;
 use TomTroc\Core\Http\Response;
 
 class Router {
+    private array $routes = [
+        'GET' => [],
+        'POST' => []
+    ];
+    public function __construct()
+    {
+        $this->routes['GET'][] = new Route('app_home', '/', fn() => (new HomeController())->index(), Method::GET);
+        $this->routes['GET'][] = new Route('app_home', '/home', fn() => (new HomeController())->index(), Method::GET);
+    }
+
     public function findRoute(Request $request): Route
     {
-        return new Route('app_home', '', fn() => (new HomeController())->index(), Method::GET);
+        foreach($this->routes[$request->method->value] as $route) {
+            $routePattern = '/^'. str_replace('/', '\/', trim($route->path, '/')) . '$/';
+            $requestUri = str_replace('/', '\/', trim($request->uri, '/'));
+            if(preg_match($routePattern, $requestUri)){
+                return $route;
+            }
+        }
+        throw new RouteNotFound();
     }
 
     public function handleRequest(Request $request)
     {
-        $route = $this->findRoute($request);
-        $response = ($route->controller)($request);
+        try {
+            $route = $this->findRoute($request);
+            $response = ($route->controller)($request);
+        } catch(RouteNotFound) {
+            $response = new Response(404, '404 - Not found');
+        } catch (Throwable) {
+            $response = new Response(500, '500 - Unexpected error');
+        }
+        
 
         if(!$response instanceof Response){
             $response = new Response(200, (string) $response);
