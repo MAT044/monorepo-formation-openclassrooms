01e11ab07b35eb2771eba69f367906f6397f1664
Vincent DORIAN <ress.edrakmisto@gmail.com>
2026-08-18 18:21:37 +0200

    [Projet06B] Intégration du header

--- projets/6_tom_troc/templates/includes/header.php
+++ projets/6_tom_troc/templates/includes/header.php
@@ -1,10 +1,20 @@
-<header>
-    <h1>Header</h1>
-    <nav>
-        <ul>
+<header class="tt-header">
+    <div class="tt-navbar">
+        <img src="/assets/logo/tom_troc.svg" alt="TomTroc" width="155px" height="51px" class="tt-logo"/>
+        <nav class="tt-menu">
+        <ul class="tt-menu-section">
             <li>
-                <a href="#">Lien A</a>
+                <a href="#" class="tt-menu-item">Accueil</a>
+            </li>
+            <li>
+                <a href="#" class="tt-menu-item">Nos livres </a>
             </li>
         </ul>
-    </nav>
+        <ul class="tt-menu-section">
+            <li><a href="#" class="tt-menu-item">Messagerie</a></li>
+            <li><a href="#" class="tt-menu-item">Mon compte</a></li>
+            <li><a href="#" class="tt-menu-item">Connexion</a></li>
+        </ul>
+        </nav>
+    </div>
 </header>
