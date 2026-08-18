a2db54072d1563015c98a982af7c8c6dc79663f0
Vincent DORIAN <ress.edrakmisto@gmail.com>
2026-08-18 18:20:49 +0200

    [Projet06B] Fix : fichiers js et css désormais exclus du frontController

--- projets/6_tom_troc/public/index.php
+++ projets/6_tom_troc/public/index.php
@@ -6,6 +6,10 @@ use TomTroc\Core\Router\Router;
 
 require dirname(__DIR__) . '/autoload.php';
 
+if (preg_match('/\.(?:png|jpg|jpeg|gif|svg|css|js)$/', $_SERVER["REQUEST_URI"])) {
+    return false;    // serve the requested resource as-is.
+}
+
 $container = new DependencyContainer(require dirname(__DIR__) . '/config/container.php');
 
 $router = $container->resolve(Router::class);
