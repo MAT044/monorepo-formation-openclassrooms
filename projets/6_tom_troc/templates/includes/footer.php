1841ea5e508958e6d155e6b6131d8a32da4fadfe
Vincent DORIAN <ress.edrakmisto@gmail.com>
2026-08-19 14:10:50 +0200

    [Projet06B] Intégration du footer

---
+++ projets/6_tom_troc/templates/includes/footer.php
@@ -0,0 +1,18 @@
+<footer>
+    <nav class="tt-navbar tt-footer-navbar">
+        <a href="/" class="tt-logo-wrapper"><img src="/assets/logo/tom_troc_minimal.svg" width="22" height="18" class="tt-logo"></a>
+        <div class="tt-menu">
+            <ul class="tt-menu-section">
+                <li>
+                    <a href="#" class="tt-menu-item">Politique de confidentialité</a>
+                </li>
+                <li>
+                    <a href="#" class="tt-menu-item">Mentions légales</a>
+                </li>
+                <li>
+                    <a href="#" class="tt-menu-item">Tom Troc©</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</footer>
