eae5ebd66a833a836ce6cbbcabf1bc6e872559b7
Vincent DORIAN <ress.edrakmisto@gmail.com>
2026-08-19 10:59:19 +0200

    [Projet06B] Fix : ajustements du header

--- projets/6_tom_troc/templates/includes/header.php
+++ projets/6_tom_troc/templates/includes/header.php
@@ -1,20 +1,23 @@
 <header class="tt-header">
-    <div class="tt-navbar">
-        <img src="/assets/logo/tom_troc.svg" alt="TomTroc" width="155px" height="51px" class="tt-logo"/>
-        <nav class="tt-menu">
-        <ul class="tt-menu-section">
-            <li>
-                <a href="#" class="tt-menu-item">Accueil</a>
-            </li>
-            <li>
-                <a href="#" class="tt-menu-item">Nos livres </a>
-            </li>
-        </ul>
-        <ul class="tt-menu-section">
-            <li><a href="#" class="tt-menu-item">Messagerie</a></li>
-            <li><a href="#" class="tt-menu-item">Mon compte</a></li>
-            <li><a href="#" class="tt-menu-item">Connexion</a></li>
-        </ul>
-        </nav>
-    </div>
+    <nav class="tt-navbar">
+        <div class="tt-container">
+            <a href="/" class="tt-logo-wrapper"><img src="/assets/logo/tom_troc.svg" alt="TomTroc" width="155px"
+                    height="51px" class="tt-logo" /></a>
+            <div class="tt-menu">
+                <ul class="tt-menu-section">
+                    <li>
+                        <a href="#" class="tt-menu-item">Accueil</a>
+                    </li>
+                    <li>
+                        <a href="#" class="tt-menu-item">Nos livres </a>
+                    </li>
+                </ul>
+                <ul class="tt-menu-section">
+                    <li><a href="#" class="tt-menu-item">Messagerie</a></li>
+                    <li><a href="#" class="tt-menu-item">Mon compte</a></li>
+                    <li><a href="#" class="tt-menu-item">Connexion</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </header>
