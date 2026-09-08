d287ec49ef51bce165a06277285ab14975f32dc6
Vincent DORIAN <ress.edrakmisto@gmail.com>
2026-09-08 22:33:27 +0200

    [Projet06B] Ajustements templates "livres"

--- projets/6_tom_troc/templates/book/info.php
+++ projets/6_tom_troc/templates/book/info.php
@@ -1,17 +1,22 @@
 
 <div class="tt-grid">
-    <img class="tt-img-cover">
+    <img src="<?= $book->illustrationUri ?>" class="tt-img-cover">
     <div class="tt-book-info">
         <h1><?= htmlspecialchars($book->title) ?></h1>
         <p>par <?= htmlspecialchars($book->author) ?></p>
+        <hr class="tt-text-separator">
 
-        <p>Description</p>
+        <h2 class="tt-description-title">DESCRIPTION</h2>
         <p><?= htmlspecialchars($book->description) ?></p>
 
-        <p>Propriétaire</p>
-        <span class="tt-pill">
-            <img src="<?=  $owner->avatarUri ?>">
-            <p><?= $owner->username ?></p>
-        </span>
+        <h2 class="tt-description-title">PROPRIETAIRE</h2>
+        <a href="/conversations/<?= $owner->id ?>" class="tt-pill">
+            <img src="<?=  $owner->avatarUri ?>" class="tt-avatar tt-avatar-s"><span class="tt-pill-body"><?= $owner->username ?></span>
+        </a>
+        <div class="tt-form-actions">
+            <a href="/conversations/<?= $owner->id ?>" class="tt-cta">
+                Envoyer un message
+            </a>
+        </div>
     </div>
 </div>
--- projets/6_tom_troc/templates/book/list.php
+++ projets/6_tom_troc/templates/book/list.php
@@ -1,11 +1,13 @@
-<div class="tt-container">
-    <h1>Livres disponibles</h1>
+<div class="tt-container-cards">
+    <h1>Nos livres à l'échange</h1>
     <div class="tt-cards-grid">
         <?php foreach($books ?? [] as $book): ?>
-            <a href="/livres/<?=  $book->id ?>" class="tt-card">
+            <a href="/livres/<?=  $book->id ?>" class="tt-card tt-book-card">
                 <img class="tt-card-img">
                 <div class="tt-card-body">
-                    <p class="tt-card-title"><?=  $book->title ?></p>
+                    <h5 class="tt-card-title"><?=  $book->title ?></h5>
+                    <p class="tt-card-subtitle"><?=  $book->author ?></p>
+                    <p class="tt-card-footnote">Vendu par : <?=  $book->author ?></p>
                 </div>
             </a>
         <?php endforeach; ?>
