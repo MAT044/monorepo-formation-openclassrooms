5394d062518e14f158430408dc509a29e60325bb
Vincent DORIAN <ress.edrakmisto@gmail.com>
2026-09-15 13:43:26 +0200

    [Projet06B] Feat : support des fichiers dans l'objet request

--- projets/6_tom_troc/src/Core/Http/Request.php
+++ projets/6_tom_troc/src/Core/Http/Request.php
@@ -8,20 +8,27 @@ class Request {
         public readonly string $uri,
         public readonly array $query,
         public readonly array $body,
+        public readonly array $files = [],
     ){}
 
     public function get(string $name, mixed $default = null)
     {
         return $this->body[$name] ?? $this->query[$name] ?? $default;
     }
+
+    public function file(string $name, mixed $default = null): ?array
+    {
+        return $this->files[$name] ?? $default;
+    }
     
     public static function fromGlobals(): Request
     {
         return new Request(
             Method::from($_SERVER['REQUEST_METHOD']),
-            $_SERVER['REQUEST_URI'],
+            strtok($_SERVER['REQUEST_URI'], '?'),
             $_GET,
-            $_POST
+            $_POST,
+            $_FILES
         );
     }
 
@@ -30,7 +37,8 @@ class Request {
             $this->method,
             $this->uri,
             array_merge($this->query, $params),
-            $this->body
+            $this->body,
+            $this->files
         );
     }
 }
